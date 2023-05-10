a942ad362f86f392f32ac5a8f192936f25028178
Ajeet <124139353+ajt11101@users.noreply.github.com>
2023-05-10 23:57:15 +0530

    10May

--- src/components/News.js
+++ src/components/News.js
@@ -5,10 +5,31 @@ export class News extends Component {
   render() {
     return (
       <div>
-        <div>
-          <Newsitem title="business" description="this is fucking news" />
-          <Newsitem />
-          <Newsitem />
+        <div className="container my-3">
+          <h2>Fresh Headlines</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <Newsitem
+                title="business"
+                description="this is fucking news"
+                imageUrl="https://i.ytimg.com/vi/VB4FTva1Bno/maxresdefault_live.jpg"
+              />
+            </div>
+            <div className="col-md-4">
+              <Newsitem
+                title="business"
+                description="this is fucking news"
+                imageUrl="https://i.ytimg.com/vi/VB4FTva1Bno/maxresdefault_live.jpg"
+              />
+            </div>
+            <div className="col-md-4">
+              <Newsitem
+                title="business"
+                description="this is fucking news"
+                imageUrl="https://i.ytimg.com/vi/VB4FTva1Bno/maxresdefault_live.jpg"
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
